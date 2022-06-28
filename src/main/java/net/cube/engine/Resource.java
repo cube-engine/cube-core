1b4469fd1223a90031b783401fd71e121986ebe5
liubing <liubing@mobike.com>
2022-06-28 10:03:49 +0800

    to #2

---
+++ src/main/java/net/cube/engine/Resource.java
@@ -0,0 +1,15 @@
+package net.cube.engine;
+
+/**
+ * @author pluto
+ * @date 2022/6/28
+ */
+public interface Resource {
+
+    /**
+     *
+     * @return
+     */
+    ResourceIdentifier getIdentifier();
+
+}
